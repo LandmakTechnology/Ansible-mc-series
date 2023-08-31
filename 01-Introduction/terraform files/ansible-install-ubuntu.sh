03581dbcecbaf0793c6cfbd401c69111927bc4ff
Kenmakhanu <73675815+Kenmakhanu@users.noreply.github.com>
2023-08-31 18:15:15 -0400

    Delete 01-Introduction directory

--- 01-Introduction/terraform files/ansible-install-ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo adduser ansible
-echo "ansible:ansible" | chpasswd
-echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
-sudo su - ansible
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt install ansible -y
--- 01-Introduction/terraform files/vscode-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt upgrade -y
-sudo apt install software-properties-common apt-transport-https wget -y
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
-sudo apt install code -y

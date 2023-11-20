75055d18afd403c763259df45e3b624c889c1746
claudedevops <142307793+claudedevops@users.noreply.github.com>
2023-11-20 06:28:24 -0800

    Update provider.tf

--- 01-Introduction/terraform files/provider.tf
+++ 01-Introduction/terraform files/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0" # Optional but recommended in production
+      version = "~> 5.0" # Optional but recommended in production
     }
   }
 }
